9336c1d956e72edf74a891832b2dda5e2c6fc859
Ebuka Francis <nwokefrancis99@gmail.com>
2023-10-03 12:48:21 +0100

    updates

---
+++ main.js
@@ -0,0 +1,11 @@
+
+function myFunction(){
+    const x = document.getElementById("myNav");
+    if (x.style.display === "block"){
+        x.style.display = "none";
+
+    }
+    else {
+        x.style.display = "block";
+    }
+}
